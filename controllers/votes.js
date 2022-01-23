33c52ed2b16bd92f3ec4cdc4b26981c02672595a
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-01-23 15:02:45 +0530

    Update votes.js

--- controllers/votes.js
+++ controllers/votes.js
@@ -24,6 +24,28 @@ exports.updateVotesOfAllUsers=(req,res)=>{
     })
 }
 
+exports.sendNameWithPercentages=(req,res)=>{
+  Contestant.find().exec((err,contestan)=>{
+    if(err){
+      return res.status(400).json({
+        error:"Unable to Fetch your req at this Time"
+      })
+    }
+    var total_votes=0;
+        var Percentagesarr=[]
+        var conteName=[]
+         contestan.map((conte,index)=>{
+           conteName.push(conte.name)
+           total_votes+=conte.votes;
+         })
+         contestan.map((conte,index)=>{
+          var tmp_contestant_per=((conte.votes)/total_votes)*100;
+          Percentagesarr.push(tmp_contestant_per);
+        })
+        res.send({conteName,Percentagesarr})
+  })
+}
+
 exports.loadUserVotes=(req,res)=>{
   User.find({_id:req.profile._id}).exec((err,data)=>{
     if(err){
