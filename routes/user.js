4bb3846febb3f84bbc763648ffe3574f39c2ee47
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-02-04 23:46:58 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,7 @@ router.param("userId", getUserById);
 
 router.get("/user/:userId/getuser", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId/updateuser", isSignedIn, isAuthenticated, updateUser);
-router.post("/user/:userId/getname",getNameById);
+router.post("/getname",getNameById);
 
 
 module.exports = router;
